d971743b3e037cc2c481e54cdddb247308cf8786
MAKIKO KARASAWA <truehope320@gmail.com>
2026-05-20 13:36:21 +0900

    JS ハンバーガーメニューの開閉の実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
 'use strict'
 
 /*******************************************
- * 
+ * ハンバーガーメニューの開閉
  * 
  *******************************************/
+const hamburger = document.getElementById('hambergar');
+const menu = document.querySelector('.hambergar__menu');
+const closeBtns = document.querySelectorAll('.close_hambergar');
+
+// メニューを開く
+hamburger.addEventListener('click',() => {
+  menu.style.transform = 'translateX(0)';
+  hamburger.style.right = '450px';
+});
+
+// メニューを閉じる
+closeBtns.forEach(closeBtn => {
+  closeBtn.addEventListener('click', ()=> {
+    menu.style.transform ='translateX(100%)';
+    hamburger.style.right = '0';
+  });
+});
