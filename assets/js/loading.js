4be9358f9cf39575f1f56f475b6c88a540c707fc
MAKIKO KARASAWA <truehope320@gmail.com>
2026-05-22 15:15:51 +0900

    トップページの微調整完了

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,19 +1,19 @@
 window.addEventListener('load', () => {
 
-  const loading = document.getElementById('loading');
-  const today = new Date().toLocaleDateString();
+  // const loading = document.getElementById('loading');
+  // const today = new Date().toLocaleDateString();
 
-  // 保存されてる日付取得
-  const savedDate = localStorage.getItem('sleLoadingDate');
+  // // 保存されてる日付取得
+  // const savedDate = localStorage.getItem('sleLoadingDate');
 
-  // 今日すでに表示済み
-  if (savedDate === today) {
-    loading.remove();
-    return;
-  }
+  // // 今日すでに表示済み
+  // if (savedDate === today) {
+  //   loading.remove();
+  //   return;
+  // }
 
-  // 今日の日付を保存
-  localStorage.setItem('sleLoadingDate', today);
+  // // 今日の日付を保存
+  // localStorage.setItem('sleLoadingDate', today);
 
   // ローディング表示時間
   setTimeout(() => {
